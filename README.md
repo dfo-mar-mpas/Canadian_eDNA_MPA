db9ad85b0f80f00c63d205c233273c8d3b7066d0
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2026-07-23 16:34:15 -0300

    maritimes readme plot

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Progress on Operationalizing eDNA Biomonitoring within Canadian Marine Conservation Areas
 
 ![](output/canada_and_regions_combined.png)
+
+#Figure 1 - Canadian Conservation Network
+
+![](output/mar_edna_readme.png)
+
+#Scotian Shelf-Bay of Fundy conservaiton network eDNA sampling program
--- code/00_readme_plot.R
+++ code/00_readme_plot.R
@@ -6,6 +6,9 @@ library(sf)
 library(rnaturalearth)
 library(patchwork)
 library(ggspatial)
+library(MarConsNetData) #go here to install https://github.com/dfo-mar-mpas/MarConsNetData
+library(marmap)
+library(terra)
 
 #map projections
 latlong <- "+init=epsg:4326 +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"
@@ -29,7 +32,7 @@ ocean_df <- bioregion_df%>%
             st_transform(CanProj)
 
 #load the cpcad_marine datafile
-cpcad_marine <- read_sf("data/shapefiles/cpcad_complete.shp")%>%
+cpcad_marine <- read_sf("data/shapefiles/cpcad_complete.shp")%>% #can also update this with MarConsNetData but it takes a looong time
                 st_transform(CanProj)%>%
                 st_make_valid()
 
@@ -182,3 +185,80 @@ full_combo_plot <- p_canada_no_legend / wrap_elements(full = combo_regions) +
   plot_layout(heights = c(1, 0.8))
 
 ggsave("output/canada_and_regions_combined.png",full_combo_plot,width = 10,height = 7.5,units = "in",dpi = 300)
+
+#maritimes region quick plot ----
+
+mar_network <- data_draft_areas()%>% #download from open data
+  filter(SiteName_E != "Northeast Channel Coral Marine Refuge")%>% #this is just overlapped by the Fundian
+  st_transform(CanProj)%>%
+  mutate(type=case_when(grepl("refuge",tolower(SiteName_E)) ~ "Marine Refuge",
+                        grepl("marine protected",tolower(SiteName_E)) ~ "MPA",
+                        TRUE ~ "Draft"),
+         type=ifelse(type == "Draft" & Classification_E == "Existing site", "OECM",type),
+         type=ifelse(SiteName_E %in% c("Fundian Channel-Browns Bank","Eastern Shore Islands"),"AOI",type),
+         type=factor(type,levels=c("MPA","Marine Refuge","OECM","AOI","Draft")))
+
+got_edna_df <- read.csv("data/edna_samples/goteDNA_July_26.csv")%>% #these are all unique coordinates ignore the taxonomy
+               st_as_sf(coords=c("decimalLongitude","decimalLatitude"),crs=latlong)%>%
+               st_transform(CanProj)
+
+mar_edna_df <- got_edna_df%>%st_intersection(mar_network) #note these are not all the coordinates for samples, we have much more from the RV survey yet processed
+
+#bounding box for the maritimes plot
+ss_bound <- bioregion_df%>%
+  filter(region == "Scotian Shelf")%>%
+  st_transform(CanProj)%>%
+  st_buffer(100*1000)%>%
+  st_bbox()
+
+#get the 250m contour
+
+atlantic_bbox_latlong <- ocean_df %>% 
+  filter(ocean == "Atlantic") %>% 
+  st_transform(4326) %>% 
+  st_bbox()
+
+atlantic_bathy <- getNOAA.bathy(
+  lon1 = atlantic_bbox_latlong["xmin"],
+  lon2 = atlantic_bbox_latlong["xmax"],
+  lat1 = atlantic_bbox_latlong["ymin"],
+  lat2 = atlantic_bbox_latlong["ymax"],
+  resolution = 1, #1 min resolution can make it smaller if needed
+  keep=TRUE,
+  path="data/bathymetry"
+)
+
+#force into an sf object
+contour_250m_sf <- atlantic_bathy%>%
+  marmap::as.raster()%>%
+  rast()%>%
+  as.contour(., levels = -250) %>% 
+  st_as_sf() %>% 
+  st_set_crs(4326) %>% 
+  st_transform(CanProj)
+
+mar_cpcad <- cpcad_marine%>%
+              filter(ocean=="Atlantic",
+                     bioregion!="Scotian Shelf")
+
+
+p1 <- ggplot()+
+  geom_sf(data=contour_250m_sf,linewidth=0.25,col="grey80")+
+  geom_sf(data=bioregion_df%>%filter(region=="Scotian Shelf"),fill=NA)+
+  geom_sf(data=basemap)+
+  geom_sf(data=basemap%>%filter(country == "Canada"),fill="grey60")+
+  geom_sf(data=mar_cpcad,aes(fill=type),alpha=0.65)+
+  geom_sf(data=mar_network,aes(fill=type),alpha=0.85,col="black")+
+  geom_sf(data=mar_edna_df,size=0.80)+
+  geom_sf(data=ss_bound%>%st_as_sfc(),fill=NA,linewidth=0.5,linetype=2)+
+  theme_bw()+
+  coord_sf(xlim=ss_bound[c(1,3)],ylim=ss_bound[c(2,4)],expand=0)+
+  labs(fill="")+
+  scale_fill_manual(values=colour_pal_types)+
+  annotation_scale(location="bl")+
+  theme(legend.position="inside",
+        legend.position.inside = c(0.87,0.15),
+        legend.title = element_blank(),
+        legend.background = element_blank())
+
+ggsave("output/mar_edna_readme.png",p1,height=6,width=6,units="in",dpi=300)
